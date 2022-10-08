74eb251cef73f6874ec9cd7eaf0a6376f0204429
Natanael <NatanaelUF17@gmail.com>
2022-10-08 10:55:52 -0400

    🧭 Bottom tab navigation added

--- clarity-app/App.tsx
+++ clarity-app/App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import Dashboard from './views/Dashboard';
+import Reports from './views/Reports';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Dashboard") {
+            return <Feather name="home" size={24} color={'white'} />
+          } else if (route.name === "Reports") {
+            return <Ionicons name="information-circle-outline" size={24} color={'white'} />
+          }
+        },
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'white',
+        tabBarStyle: {
+          backgroundColor: '#3B70EE'
+        }
+      })}>
+        <Tab.Screen
+          name='Dashboard'
+          component={Dashboard}
+          options={{
+            headerStyle: styles.dashboardHeaderStyle,
+            headerShown: true
+          }}
+        />
+        <Tab.Screen
+          name='Reports'
+          component={Reports}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  dashboardHeaderStyle: {
+    backgroundColor: 'white',
+    shadowColor: "#0000000F",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.55,
+    shadowRadius: 5,
+    elevation: 21,
+  }
 });
